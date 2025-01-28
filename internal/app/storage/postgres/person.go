7a085867bc0221eeaaba5fa034d0c8e1515f43bb
Muaz717 <m.azizov03@mail.ru>
2025-01-28 13:49:24 +0300

    Third commit

--- internal/app/storage/postgres/person.go
+++ internal/app/storage/postgres/person.go
@@ -2,7 +2,10 @@ package postgres
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/jackc/pgx/v5"
+	"gym_app/internal/app/storage"
 	"gym_app/internal/models"
 )
 
@@ -23,3 +26,21 @@ func (s *Storage) savePerson(
 
 	return personId, nil
 }
+
+func (s *Storage) findAllPeople(ctx context.Context) ([]models.Person, error) {
+	const op = "postgres.findAllPeople"
+
+	query := `SELECT id, `
+
+	rows, err := s.db.Query(ctx, query)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return pgx.CollectRows(rows, pgx.RowToStructByName[models.Person])
+}
+
+//func (s *Storage)
